7b47972dd89d350978ee5b5b280218fa4af1565b
Kirill Polushin <kirill@polushin.org>
2018-11-06 22:34:40 +0700

    Refactor cache header

--- cache.h
+++ cache.h
@@ -1,27 +1,32 @@
 
 #include <stdbool.h>
+#include <stddef.h>
 
 #ifndef _CACHE_H
 #define _CACHE_H
 
-typedef struct cache_entry_struct {
-  struct cache_entry_struct* next;
-  char* path;
-  char* mime_type;
+typedef struct cache_entry_reader {
+  void (*callback)(char* data, size_t len);
+  struct cache_entry_reader* next;
+} cache_entry_reader_t;
+
+typedef struct cache_entry {
+  char* url;
+  bool finished;
+  size_t len;
   char* data;
+  cache_entry_reader_t* readers;
+  struct cache_entry* next;
 } cache_entry_t;
 
-typedef struct cache_struct {
+typedef struct cache {
+  cache_entry_t* list;
 } cache_t;
 
 bool cache_init(cache_t* cache);
 
-bool cache_insert_entry(cache_t* cache, cache_entry_t* entry);
+int cache_find_or_create(cache_t* cache, char* url, cache_entry_t** entry);
 
 void cache_free(cache_t* cache);
-
-bool cache_entry_init(cache_entry_t* entry, char* path, char* mime_type);
-
-void cache_entry_free(cache_entry_t* entry);
 
 #endif
